9a651a85101a78166bca23b8e23ca86574c34600
sheelfshah <sheelfshah@gmail.com>
2021-03-26 22:10:34 +0530

    idr remember

--- network.py
+++ network.py
@@ -1,29 +1,33 @@
 import socket
 import pickle
 
-#172.87.175.81
+# 172.87.175.81
+
+
 class Network:
-	def __init__(self):
-		self.client= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.server="20.184.24.226"
-		self.port=5555
-		self.addr=(self.server, self.port)
-		self.p=self.connect()	#tells what player you are
 
-	def connect(self):
-		try:
-			self.client.connect(self.addr)
-			return self.client.recv(2048).decode()
-		except:
-			pass
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server = "192.168.0.150"
+        self.port = 5555
+        self.addr = (self.server, self.port)
+        self.p = self.connect()  # tells what player you are
 
-	def send(self, data):
-		#sends what to do, receives state of game as response and returns it to client
-		try:
-			self.client.send(str.encode(data))
-			return pickle.loads(self.client.recv(4096))
-		except socket.error as e:
-			print(e)
+    def connect(self):
+        try:
+            self.client.connect(self.addr)
+            return self.client.recv(2048).decode()
+        except:
+            pass
 
-	def getP(self):
-		return self.p
+    def send(self, data):
+        # sends what to do, receives state of game as response and returns it
+        # to client
+        try:
+            self.client.send(str.encode(data))
+            return pickle.loads(self.client.recv(4096))
+        except socket.error as e:
+            print(e)
+
+    def getP(self):
+        return self.p
